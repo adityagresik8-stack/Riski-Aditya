f5c9bcffe1a75154870231aa99cb2c3c6e2c2cf3
adityagresik8-stack <adityagresik8@gmail.com>
2025-10-16 13:36:18 +0700

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,7 @@
+//1.pilih tombol berdasarkan ID-nya
+conts tombolsapa=documen.getElemenById('sapaButton);
+//2. Tambahkan 'even listener' untuk aksi 'click'
+ tombolSapa.addEventListener('click', function() {
+  //3. jalankan kode ini ketika tombol di-klik
+  alert('Halo! Terima kasih sudah mengunjungi!');
+});
